ee9c82c4e39e5c7c58af3f4beec10ea6e5711f8e
Jimmy Hoang <60477747+JimmyHoang296@users.noreply.github.com>
2023-04-21 11:13:55 +0700

    Update ass.js

--- src/components/ass.js
+++ src/components/ass.js
@@ -1,6 +1,6 @@
 export default function convertToLocalDate(dateString) {
   const mDate = new Date(dateString).toLocaleDateString();
-  const [m, d, y] = mDate.split("/");
+  const [d, m, y] = mDate.split("/");
 
   return `${d < 10 ? "0" + d : d}/${m < 10 ? "0" + m : m}/${y}`;
 }
