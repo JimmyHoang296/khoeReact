603e607d91c5358a56fefba564b5bfa88a77c838
Jimmy Hoang <60477747+JimmyHoang296@users.noreply.github.com>
2024-10-15 22:45:37 +0700

    Update variable.js
    fix bug when create le ticket

--- src/components/variable.js
+++ src/components/variable.js
@@ -1,3 +1,4 @@
 const URL =
-  "https://script.google.com/macros/s/AKfycbxBMqQuGQXb4yUDgyTGdHp6Hj_iUvaeTclAIVh3de5DC_MtoSzu1oy6_uz9uA0g4IKj/exec";
+  // "https://script.google.com/macros/s/AKfycbxBMqQuGQXb4yUDgyTGdHp6Hj_iUvaeTclAIVh3de5DC_MtoSzu1oy6_uz9uA0g4IKj/exec";
+  "https://script.google.com/macros/s/AKfycby1z6QDeCT5VuavrbIcEfIG6oUpqxQQCd8uoqFKuq1c19-2z3ojog8-VIZ0eBd_FJDs/exec";
 export default URL;
